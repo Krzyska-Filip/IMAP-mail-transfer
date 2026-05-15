2d12e288d90339aeb024b9c2675b04ac226b2076
Krzyśka Filip <45741043+Krzyska-Filip@users.noreply.github.com>
2026-05-15 20:43:45 +0200

    Simplify imap functions

--- imap.c
+++ imap.c
@@ -29,90 +29,61 @@ static size_t read_callback(char *ptr, size_t size, size_t nmemb, void *userdata
     return to_copy;
 }
 
-CURLcode imap_list_uids(struct ImapServer srv, struct Buffer *buf) {
+static CURLcode imap_execute(struct ImapServer srv, struct ImapRequest req, const char *label) {
     char url[512];
-    snprintf(url, sizeof(url), "imap://%s:%d/%s", srv.host, srv.port, srv.mailbox);
+    if (req.uid > 0)
+        snprintf(url, sizeof(url), "imap://%s:%d/%s/;UID=%d", srv.host, srv.port, srv.mailbox, req.uid);
+    else
+        snprintf(url, sizeof(url), "imap://%s:%d/%s", srv.host, srv.port, srv.mailbox);
 
     CURL *curl = curl_easy_init();
     if (!curl) return CURLE_FAILED_INIT;
-
     curl_easy_setopt(curl, CURLOPT_URL, url);
     curl_easy_setopt(curl, CURLOPT_USERNAME, srv.user);
     curl_easy_setopt(curl, CURLOPT_PASSWORD, srv.pass);
-    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "UID SEARCH ALL");
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, buf);
+    if (req.out) {
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, req.out);
+    }
+    if (req.upload) {
+        curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
+        curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_callback);
+        curl_easy_setopt(curl, CURLOPT_READDATA, req.upload);
+        curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE, (curl_off_t)req.upload->size);
+    }
+    if (req.custom_request)
+        curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, req.custom_request);
 
     CURLcode res = curl_easy_perform(curl);
     if (res != CURLE_OK)
-        fprintf(stderr, "imap_list_uids: %s\n", curl_easy_strerror(res));
-
+        fprintf(stderr, "%s: %s\n", label, curl_easy_strerror(res));
     curl_easy_cleanup(curl);
     return res;
+}
+
+CURLcode imap_list_uids(struct ImapServer srv, struct Buffer *buf) {
+    struct ImapRequest req = {0};
+    req.custom_request = "UID SEARCH ALL";
+    req.out = buf;
+    return imap_execute(srv, req, "imap_list_uids");
 }
 
 CURLcode imap_fetch_message(struct ImapServer srv, int uid, struct Buffer *buf) {
-    char url[512];
-    snprintf(url, sizeof(url), "imap://%s:%d/%s/;UID=%d", srv.host, srv.port, srv.mailbox, uid);
-
-    CURL *curl = curl_easy_init();
-    if (!curl) return CURLE_FAILED_INIT;
-
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_USERNAME, srv.user);
-    curl_easy_setopt(curl, CURLOPT_PASSWORD, srv.pass);
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, buf);
-
-    CURLcode res = curl_easy_perform(curl);
-    if (res != CURLE_OK)
-        fprintf(stderr, "imap_fetch_message UID=%d: %s\n", uid, curl_easy_strerror(res));
-
-    curl_easy_cleanup(curl);
-    return res;
+    struct ImapRequest req = {0};
+    req.uid = uid;
+    req.out = buf;
+    return imap_execute(srv, req, "imap_fetch_message");
 }
 
 CURLcode imap_append_message(struct ImapServer dst, struct Buffer msg) {
-    char url[512];
-    snprintf(url, sizeof(url), "imap://%s:%d/%s", dst.host, dst.port, dst.mailbox);
-
-    CURL *curl = curl_easy_init();
-    if (!curl) return CURLE_FAILED_INIT;
-
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_USERNAME, dst.user);
-    curl_easy_setopt(curl, CURLOPT_PASSWORD, dst.pass);
-    curl_easy_setopt(curl, CURLOPT_UPLOAD, 1L);
-    curl_easy_setopt(curl, CURLOPT_READFUNCTION, read_callback);
-    curl_easy_setopt(curl, CURLOPT_READDATA, &msg);
-    curl_easy_setopt(curl, CURLOPT_INFILESIZE_LARGE, (curl_off_t)msg.size);
-
-    CURLcode res = curl_easy_perform(curl);
-    if (res != CURLE_OK)
-        fprintf(stderr, "imap_append_message: %s\n", curl_easy_strerror(res));
-
-    curl_easy_cleanup(curl);
-    return res;
+    struct ImapRequest req = {0};
+    req.upload = &msg;
+    return imap_execute(dst, req, "imap_append_message");
 }
 
 CURLcode imap_fetch_envelopes(struct ImapServer srv, struct Buffer *buf) {
-    char url[512];
-    snprintf(url, sizeof(url), "imap://%s:%d/%s", srv.host, srv.port, srv.mailbox);
-
-    CURL *curl = curl_easy_init();
-    if (!curl) return CURLE_FAILED_INIT;
-
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-    curl_easy_setopt(curl, CURLOPT_USERNAME, srv.user);
-    curl_easy_setopt(curl, CURLOPT_PASSWORD, srv.pass);
-    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "UID FETCH 1:* (ENVELOPE)");
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, buf);
-
-    CURLcode res = curl_easy_perform(curl);
-    if (res != CURLE_OK)
-        fprintf(stderr, "imap_fetch_envelopes: %s\n", curl_easy_strerror(res));
-
-    curl_easy_cleanup(curl);
-    return res;
+    struct ImapRequest req = {0};
+    req.custom_request = "UID FETCH 1:* (ENVELOPE)";
+    req.out = buf;
+    return imap_execute(srv, req, "imap_fetch_envelopes");
 }
--- imap.h
+++ imap.h
@@ -16,6 +16,13 @@ struct Buffer {
     size_t size;
 };
 
+struct ImapRequest {
+    const char    *custom_request;
+    int            uid;
+    struct Buffer *out;
+    struct Buffer *upload;
+};
+
 CURLcode imap_list_uids(struct ImapServer srv, struct Buffer *buf);
 CURLcode imap_fetch_message(struct ImapServer srv, int uid, struct Buffer *buf);
 CURLcode imap_append_message(struct ImapServer dst, struct Buffer msg);
